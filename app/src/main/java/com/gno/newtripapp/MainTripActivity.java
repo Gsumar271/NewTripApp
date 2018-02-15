48a645cf2b77c791dc5750d3eb2d105d16a46d10
GeneSumar <statsrats@yahoo.com>
2018-02-15 06:22:17 +0200

    Update MainTripActivity.java

--- app/src/main/java/com/gno/newtripapp/MainTripActivity.java
+++ app/src/main/java/com/gno/newtripapp/MainTripActivity.java
@@ -51,24 +51,13 @@ public class MainTripActivity extends AppCompatActivity
     private DaysAdapter da;
     private EventItemAdapter eia;
 
-
-  //  private List<String> trips;
-   // private ArrayList<Integer> daysInt;
     private ArrayList<String> days;
-   // private String tripName="";
-   // private ArrayList<EventItem> allEvents;
-
-
-
     private ArrayList<EventItem> events;
     private String whichButtonClicked;
 
     private ArrayList <Trip> allTrips;
     private Trip trip;
     private int day;
-
-
-   // private int dayCounter = 0;
     private int resID;
 
     FragmentManager fm = getFragmentManager();
@@ -113,16 +102,12 @@ public class MainTripActivity extends AppCompatActivity
 
         FragmentManager fm = getFragmentManager();
 
-       // itemFragment = (ItemFragment) fm.findFragmentById(R.id.ItemFragment);
-
         // Create the arraylist of Trips
         allTrips = new ArrayList<Trip>();
         //Create arraylist of Days
         days = new ArrayList<String>();
         //Create arraylist of Events to use for each day
         events = new ArrayList<EventItem>();
-        //Create arrayList of all the Events
-      //  allEvents = new ArrayList<EventItem>();
         //Create a default trip
         trip = new Trip("",0);
         day = 0;
@@ -148,15 +133,6 @@ public class MainTripActivity extends AppCompatActivity
             public void onClick(View v) {
                 switch (itemType){
                     case TRIP:
-                        /*
-                        String name =  "New Trip " + dayCounter;
-                        Trip thisTrip = new Trip(name, dayCounter);
-                        //insert trip into alltrips array
-                        allTrips.add(thisTrip);
-                        dayCounter++;
-                        tna.notifyDataSetChanged();
-                        updateTrip(thisTrip);
-                        */
                         openTripPickerDialog(false);
                         break;
                     case DAY:
@@ -165,8 +141,6 @@ public class MainTripActivity extends AppCompatActivity
                         //update the fragment view
                         da.newItems(trip.getNumDays());
                         updateTrip(trip);
-                        //  Log.v("secndnumofDays", ""+days.size());
-                      //  dayCounter++;
                         break;
                     case EVENT:
                         //get the name of the trip and the number of days
@@ -175,7 +149,7 @@ public class MainTripActivity extends AppCompatActivity
 
                         EventItem event = new EventItem("New Event", tripName2, numOfDays2);
                         events.add(event);
-                     //   allEvents.add(event);
+
                         updateAllEvents(event);
 
                         eia.notifyDataSetChanged();
@@ -190,8 +164,7 @@ public class MainTripActivity extends AppCompatActivity
     }
 
     public void openTripPickerDialog(boolean shouldCheck){
-       // long initialDays = 0;
-       // String initialButton = "";
+     
         boolean mustShow = true;
 
         //this checks whether dialog should be displayed
@@ -205,7 +178,6 @@ public class MainTripActivity extends AppCompatActivity
 
 
         // Create the fragment and show it as a dialog.
-       // DialogFragment newTripFragment = TripPickerDialog.newInstance(initialDays, initialButton);
         //mustshow indicates dialog must be shown
         if(mustShow) {
             DialogFragment newTripFragment = new TripPickerDialog();
@@ -215,13 +187,12 @@ public class MainTripActivity extends AppCompatActivity
 
     public void onDatePickerClicked(View v, String whichButton){
 
-       // whichButtonClicked = whichButton;
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         //remove current trip dialog
         Fragment prevDialog = getFragmentManager().findFragmentByTag("trip_dialog");
         ft.remove(prevDialog);
         ft.addToBackStack(null);
-       // ft.commit();
+     
 
         //Create and show dialog
         DatePickerFragment dpf = new DatePickerFragment();
@@ -233,37 +204,23 @@ public class MainTripActivity extends AppCompatActivity
 
     public void onNewDateSelected(Date dateSelected, String whichButton)
     {
-      // long newDate = dateSelected.getTime();
 
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         //remove current trip dialog
         Fragment prevDialog = getFragmentManager().findFragmentByTag("datepick_dialog");
         ft.remove(prevDialog);
-        //ft.addToBackStack(null);
-        //getFragmentManager().popBackStack();
-
+     
         TripPickerDialog tpd =(TripPickerDialog) getFragmentManager().findFragmentByTag("trip_dialog");
         tpd.updateButtons(dateSelected, whichButton);
 
         getFragmentManager().popBackStack();
-
-        //Create and show dialog
-       // DialogFragment newTripFragment = TripPickerDialog.newInstance(newDate, whichButton);
-       // newTripFragment.show(getFragmentManager(), "trip_dialog");
-
-
     }
 
     public void onNewTripSelected(String _trip, String fromDate, String toDate) {
         int _numDays;
         Trip newTrip;
 
-      /*
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prevDialog = getFragmentManager().findFragmentByTag("trip_dialog");
-        ft.remove(prevDialog);
-        ft.commit();
-        */
+    
 
         //Convert returned String values to Dates
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
@@ -282,16 +239,10 @@ public class MainTripActivity extends AppCompatActivity
             newTrip = new Trip(_trip, _numDays);
 
             updateTrip(newTrip);
-
-         //   Log.v("newtrip", newTrip.getTripName());
         }
         catch (ParseException pEx){
             Log.e("error", pEx.getMessage());
         }
-
-      //  newTrip = new Trip(_trip, 0);
-
-      //  updateTrip(newTrip);
     }
 
 
@@ -302,7 +253,6 @@ public class MainTripActivity extends AppCompatActivity
         trip = allTrips.get(itemsId);
         //get the list of integers in that trip
         days = trip.getNumDays();
-      //  Log.v("firstnumofDays", ""+days.size());
 
         DayItemFragment dayItemFragment = new DayItemFragment();
 
@@ -328,12 +278,9 @@ public class MainTripActivity extends AppCompatActivity
     @Override
     public void onNewDaySelected(int itemsId) {
 
-
         events.clear();
-
         day = itemsId;
-
-
+        
         EventItemFragment eif = new EventItemFragment();
 
         resID = R.layout.event_list_row;
@@ -353,8 +300,6 @@ public class MainTripActivity extends AppCompatActivity
         itemType = ItemType.EVENT;
 
         getLoaderManager().restartLoader(1, null, this);
-
-      //  eia.notifyDataSetChanged();
     }
 
     @Override
@@ -394,7 +339,6 @@ public class MainTripActivity extends AppCompatActivity
                 cr.insert(TripContentProvider.CONTENT_URI_TRIPS, values);
                 break;
             case DAY:
-               // String where = TripContentProvider.KEY_TRIP_NAME + " = " + trip.getTripName();
                 String where = TripContentProvider.KEY_TRIP_NAME + " LIKE ? ";
                 String[] args = {trip.getTripName()};
                 values.put(TripContentProvider.KEY_NUM_OF_DAYS, trip.getDays());
@@ -433,34 +377,12 @@ public class MainTripActivity extends AppCompatActivity
 
 
         query.close();
-        //  Log.v("inserting name: ", event.getTripName());
-         //Log.v("inserting day: ", ""+event.getDay());
 
-       // events.clear();
         getLoaderManager().restartLoader(0, null, this);
 
         eia.notifyDataSetChanged();
-       // getFragmentManager().popBackStack();
 
     }
-
-
-    private void updateDays(String _trip){
-
-        /*
-
-        String tripName = _trip;
-        Integer days = Integer.valueOf(allTrips.get(tripName));
-        resID = R.layout.day_list_row;
-        da = new DaysAdapter(this, resID, days);
-        itemFragment.setListAdapter(da);
-        itemType = ItemType.DAY;
-
-        */
-
-
-    }
-
 
     //Starts the DetailsActivity using the Intent
     private void startDetailsActivity(EventItem eventItem, int position){
@@ -485,8 +407,6 @@ public class MainTripActivity extends AppCompatActivity
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        //	Log.v("ResultCode: ", String.valueOf(requestCode));
-
         if (resultCode == Activity.RESULT_OK) {
 
             // During initial setup, plug in the details fragment.
@@ -502,10 +422,6 @@ public class MainTripActivity extends AppCompatActivity
             newEventItem.created = data.getExtras().getLong("created");
 
             itemType= ItemType.EVENT;
-
-          //  Log.v("itemtype: ", "not event");
-
-           // Log.v("itemtype: ", "is event");
 
             updateAllEvents(newEventItem);
 
@@ -523,10 +439,6 @@ public class MainTripActivity extends AppCompatActivity
 
     public void deleteEvent(long _eventCreated, int position) {
 
-
-        //EventItem _event = eventItems.get(position);
-
-
         if (position != -1) {
             events.remove(position);
             eia.notifyDataSetChanged();
@@ -535,16 +447,13 @@ public class MainTripActivity extends AppCompatActivity
 
 
             String where = TripContentProvider.KEY_CREATED + " = " + _eventCreated;
-            // String[] whereArgs = new String[] { note.getTask() };
-
+       
             Cursor query = cr.query(TripContentProvider.CONTENT_URI_EVENTITEMS, null, where, null, null);
 
             if (query.getCount() != 0) {
 
                 cr.delete(TripContentProvider.CONTENT_URI_EVENTITEMS, where, null);
             }
-
-
             query.close();
         }
 
@@ -590,14 +499,7 @@ public class MainTripActivity extends AppCompatActivity
 
             }
 
-            //creating a temp List in order to add all the keys
-            //to arraylist and update the view
-            //List<String> tempList = new ArrayList<String>(allTrips.keySet());
-            // trips.addAll(tempList);
             tna.notifyDataSetChanged();
-
-
-
         }
 
         if (itemType == ItemType.EVENT ) {
@@ -610,18 +512,15 @@ public class MainTripActivity extends AppCompatActivity
             int keyDayIndex = cursor.getColumnIndexOrThrow(TripContentProvider.KEY_DAY);
 
             events.clear();
-             Log.v("events clear: ", "clear");
             while (cursor.moveToNext()) {
                 EventItem newItem = new EventItem(cursor.getString(keyEventIndex), cursor.getInt(keyTimeFromIndex),
                         cursor.getInt(keyTimeToIndex), cursor.getString(keyTripNameIndex),
                         cursor.getInt(keyDayIndex), cursor.getLong(keyCreatedIndex));
                 events.add(newItem);
-                Log.v("events clear: ", "build");
             }
             eia.notifyDataSetChanged();
         }
-
-        cursor.close();
+       cursor.close();
     }
 
 
@@ -646,196 +545,7 @@ public class MainTripActivity extends AppCompatActivity
         //  getMenuInflater().inflate(R.menu.event, menu);
         return true;
     }
-    
-
+   
 }
 
 
-// The version of activity that uses BaseAdapter and regular int instead of a list
-/*
-
- @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        FragmentManager fm = getFragmentManager();
-
-        itemFragment = (ItemFragment) fm.findFragmentById(R.id.ItemFragment);
-
-        // Create the array list of items
-
-        //initiate a HashMap to associate trip names with number of days
-        allTrips = new ArrayList<Trip>();
-
-        //list all the trips in the hashmap
-       // trips = new ArrayList<String>(allTrips.keySet());
-
-
-       //trips = new ArrayList<String>();
-
-       // days = allTrips.get(tripName);
-        days = new ArrayList<Integer>();;
-        events = new ArrayList<EventItem>();
-        allEvents = new ArrayList<EventItem>();
-
-
-        switch (itemType) {
-            case TRIP:
-                //Create the array adapter to bind the array to the listview
-                resID = R.layout.trip_name;
-                tna = new TripNameAdapter(this, resID, allTrips);
-                //Bind the array adapter to the listview/listfragment .
-                itemFragment.setListAdapter(tna);
-                break;
-            case DAY:
-                //Create the array adapter to bind the array to the listview
-                resID = R.layout.day_list_row;
-                da = new DaysAdapter(this, resID, days);
-                //Bind the array adapter to the listview/listfragment .
-                itemFragment.setListAdapter(da);
-                break;
-            case EVENT:
-                //Create the array adapter to bind the array to the listview
-                resID = R.layout.event_list_row;
-                eia = new EventItemAdapter(this, resID, events);
-                //Bind the array adapter to the listview/listfragment .
-                itemFragment.setListAdapter(eia);
-                break;
-        }
-
-     //   getLoaderManager().initLoader(0, null, this);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (itemType){
-                    case TRIP:
-                        String trip = "New Trip" + dayCounter;
-                        //insert trip into array
-                        trips.add(trip);
-                        //insert trip into hashmap to associate it with the number of days
-                        allTrips.put(trip, dayCounter);
-                        daysInTrip = dayCounter;
-                        dayCounter++;
-                        tna.notifyDataSetChanged();
-                        break;
-                    case DAY:
-                        //retrieve the trip name
-                        String tripName = trips.get(daysInTrip);
-
-                        //retrieve the number of days in that trip
-                        int numOfDays = allTrips.get(tripName);
-
-                        numOfDays++;
-                        //put new number of days in the trip hashmap
-                        allTrips.put(tripName, numOfDays);
-
-
-                        //update Integer object to which adapter has reference to
-                        days = numOfDays;
-
-                        //notify the adapter of changed array
-                        da.notifyDataSetChanged();
-
-                        dayCounter++;
-                        break;
-                    case EVENT:
-                        //get the name of the trip and the number of days
-                        String tripName2 = trips.get(daysInTrip);
-                        int numOfDays2 = allTrips.get(tripName2);
-
-                        EventItem event = new EventItem("New Event", tripName2, numOfDays2);
-                        events.add(event);
-                        allEvents.add(event);
-
-                        eia.notifyDataSetChanged();
-                        break;
-                }
-
-            }
-        });
-    }
-
-    @Override
-    public void onNewItemSelected(int itemsId) {
-
-        switch (itemType) {
-            case TRIP:
-                tripName = trips.get(itemsId);
-                days = allTrips.get(tripName);
-
-                resID = R.layout.day_list_row;
-                da = new DaysAdapter(this, resID, days);
-                itemFragment.setListAdapter(da);
-                itemType = ItemType.DAY;
-
-                da.notifyDataSetChanged();
-               // updateDays(trip);
-                break;
-            case DAY:
-               // int day = itemsId;
-               // tripName = trips.get(itemsId);
-
-                //build events arraylist from allevents to match the tripName and particular day
-                for(EventItem event : allEvents)
-                {
-                    if( event.tripName == tripName  && event.day == days)
-                        events.add(event);
-                }
-                resID = R.layout.event_list_row;
-                eia = new EventItemAdapter(this, resID, events);
-                itemFragment.setListAdapter(eia);
-
-                itemType = ItemType.EVENT;
-                eia.notifyDataSetChanged();
-
-                //changeFragment(itemsId);
-
-                break;
-
-            case EVENT:
-                EventItem event = events.get(itemsId);
-                //extract todoItem and send it to this method
-                startDetailsActivity(event, itemsId);
-                itemType = ItemType.TRIP;
-                break;
-        }
-    }
-
- */
-/*
-        switch (itemType) {
-            case TRIP:
-                //Create the array adapter to bind the array to the listview
-             //   resID = R.layout.trip_name;
-             //   tna = new TripNameAdapter(this, resID, allTrips);
-                //Bind the array adapter to the listview/listfragment .
-             //   itemFragment.setListAdapter(tna);
-
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.fragment_container, itemFragment);
-                ft.commit();
-                break;
-            case DAY:
-                //Create the array adapter to bind the array to the listview
-                resID = R.layout.day_list_row;
-                da = new DaysAdapter(this, resID, days);
-                //Bind the array adapter to the listview/listfragment .
-                itemFragment.setListAdapter(da);
-                break;
-            case EVENT:
-                //Create the array adapter to bind the array to the listview
-                resID = R.layout.event_list_row;
-                eia = new EventItemAdapter(this, resID, events);
-                //Bind the array adapter to the listview/listfragment .
-                itemFragment.setListAdapter(eia);
-                break;
-        }
-        */
